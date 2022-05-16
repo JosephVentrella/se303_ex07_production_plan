9a63d44eb5cf20b8d169915ff292a30038578fc2
JosephVentrella <ventrejo@oregonstate.edu>
2022-05-16 16:29:24 -0700

    Implement test for no Producers Test the not happy path when the producers collection is empty

--- test/province_test.rb
+++ test/province_test.rb
@@ -22,4 +22,15 @@ class ProvinceTest < Minitest::Test
     assert_equal(-6, asia.shortfall)
     assert_equal(292, asia.profit)
   end
+
+  def test_no_producers
+    data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    }
+    noProducers = Province.new(data)
+    assert_equal(30, noProducers.shortfall)
+  end
 end
