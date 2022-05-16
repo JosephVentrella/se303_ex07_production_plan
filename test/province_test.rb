207de6f46dd09015e536f4a0988ade8665677fbe
JosephVentrella <ventrejo@oregonstate.edu>
2022-05-16 16:51:54 -0700

    Implement noProducers_profit test Test when profit method is called with no producers

--- test/province_test.rb
+++ test/province_test.rb
@@ -33,4 +33,14 @@ class ProvinceTest < Minitest::Test
     noProducers = Province.new(data)
     assert_equal(30, noProducers.shortfall)
   end
+  def test_no_producers_profit
+    data = {
+  		name: "No producers",
+  		producers: [],
+  		demand: 30,
+  		price: 20
+  	}
+  	noProducers = Province.new(data)
+  	assert_equal(0, noProducers.profit)
+  end
 end
