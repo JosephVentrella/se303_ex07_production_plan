27be437b4b5cd1a840e57ddd25d0418ff186bad3
JosephVentrella <ventrejo@oregonstate.edu>
2022-05-16 17:22:56 -0700

    Add commented out empty_string tests Running these throws an error that I am not sure how to work around. It doesn't like doing subtraction with strings

--- test/province_test.rb
+++ test/province_test.rb
@@ -68,4 +68,16 @@ class ProvinceTest < Minitest::Test
   	asia.demand = -1
   	assert_equal(-10, asia.profit)
   end
+
+  def test_empty_string_demand_shortfall
+  	asia = Province.new(sample_province_data)
+  	asia.demand = ""
+  	assert_equal(nil, asia.shortfall)
+  end
+
+  def test_empty_string_demand_shortfall
+  	asia = Province.new(sample_province_data)
+  	asia.demand = ""
+  	assert_equal(nil, asia.profit)
+  end
 end
